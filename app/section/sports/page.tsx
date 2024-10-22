144fd51c20541302166a3feeb089a66c9ec4f1ce
Samuel Aragon <samuelaragon616@gmail.com>
2024-10-22 19:49:45 -0300

    update build

--- app/section/sports/page.tsx
+++ app/section/sports/page.tsx
@@ -2,14 +2,7 @@
 
 import { useState } from "react";
 import Header from "../../../components/Header";
-import Banner from "../../../components/Banner";
-import ProductCard from "../../../components/ProductCard";
 import Sidebar from "../../../components/Sidebar";
-import banner2 from "/public/mens/banner2.webp";
-import women2 from "/public/women2.png";
-import women from "/public/women.png";
-import men1 from "/public/mens/men1.png";
-import camperas from "/public/camperas.png";
 
 const News: React.FC = () => {
   const [toggle, setToggle] = useState<boolean>(false);
@@ -22,74 +15,6 @@ const News: React.FC = () => {
         </p>
       </div>
       <Header />
-
-      <div className="lg:hidden">
-        <Banner src={banner2} alt="Banner Hombres" />
-        <div className="flex m-2 justify-around gap-2 mt-10">
-          <ProductCard
-            imageSrc={women2}
-            title="Remera"
-            price={"500"}
-            brand="brand"
-          />
-          <ProductCard
-            imageSrc={women}
-            title="Remera"
-            price={"500"}
-            brand="brand"
-          />
-        </div>
-        <Banner src={banner2} alt="Banner Hombres" />
-        <div className="flex m-2 justify-around gap-2 mt-10">
-          <ProductCard
-            imageSrc={men1}
-            title="Remera"
-            price={"500"}
-            brand="brand"
-          />
-          <ProductCard
-            imageSrc={camperas}
-            title="Remera"
-            price={"500"}
-            brand="brand"
-          />
-        </div>
-      </div>
-
-      <div className="lg:w-screen lg:min-h-screen lg:bg-slate-300 lg:flex lg:justify-center hidden">
-        <div className="lg:w-[70%] lg:bg-white lg:min-h-screen lg:border-l-2 lg:border-r-2 lg:border-black">
-          <Banner src={banner2} alt="Banner Hombres" />
-          <div className="flex m-2 justify-around gap-2 mt-10">
-            <ProductCard
-              imageSrc={women2}
-              title="Remera"
-              price={"500"}
-              brand="brand"
-            />
-            <ProductCard
-              imageSrc={women}
-              title="Remera"
-              price={"500"}
-              brand="brand"
-            />
-          </div>
-          <Banner src={banner2} alt="Banner Hombres" />
-          <div className="flex m-2 justify-around gap-2 mt-10">
-            <ProductCard
-              imageSrc={men1}
-              title="Remera"
-              price={"500"}
-              brand="brand"
-            />
-            <ProductCard
-              imageSrc={camperas}
-              title="Remera"
-              price={"500"}
-              brand="brand"
-            />
-          </div>
-        </div>
-      </div>
 
       <Sidebar isOpenSideBar={toggle} onClose={() => setToggle(false)} />
     </main>
